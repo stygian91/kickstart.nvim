3eed3e9c4ee4457ba5576b8f330f676d94cbb310
Stygian <kioravuser@gmail.com>
2023-08-15 01:15:15 +0300

    Move plugin config to separate files.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,32 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- local function req(name)
+--   return require('custom.plugins.'..name)
+-- end
 --
--- See the kickstart.nvim README for more information
-return {}
+-- local names = {
+--   'telescope',
+--   'treesitter',
+-- }
+
+local exports = {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
+
+-- for _, name in pairs(names) do
+--   exports[#exports+1] = req(name)
+-- end
+
+return exports
+
---
+++ lua/custom/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+-- Fuzzy Finder (files, lsp, etc)
+return {
+  -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  build = ':TSUpdate',
+}
