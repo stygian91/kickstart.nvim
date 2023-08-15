827a79e8e54f864ff3a5def686172ddc1c8903ab
stygian91 <kioravuser@gmail.com>
2023-08-16 00:39:21 +0300

    Update treesitter move and select objects; add keybinds for goto prev/next git hunks

--- init.lua
+++ init.lua
@@ -93,25 +93,28 @@ require('nvim-treesitter.configs').setup {
         ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
         ['ic'] = '@class.inner',
+        ['am'] = '@comment.outer',
       },
     },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        [']m'] = '@function.outer',
+        [']f'] = '@function.outer',
         [']]'] = '@class.outer',
+        [']a'] = '@parameter.inner',
       },
       goto_next_end = {
-        [']M'] = '@function.outer',
+        [']F'] = '@function.outer',
         [']['] = '@class.outer',
       },
       goto_previous_start = {
-        ['[m'] = '@function.outer',
+        ['[f'] = '@function.outer',
         ['[['] = '@class.outer',
+        ['[a'] = '@parameter.inner',
       },
       goto_previous_end = {
-        ['[M'] = '@function.outer',
+        ['[F'] = '@function.outer',
         ['[]'] = '@class.outer',
       },
     },
