960b0ac284cc67a64ff67a6b24d36ac78c52c08f
Atanas Vasilev <atanasvasilev@htmlbox.net>
2023-08-16 11:11:04 +0300

    LSP: add tsserver and html

--- init.lua
+++ init.lua
@@ -164,8 +164,8 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
