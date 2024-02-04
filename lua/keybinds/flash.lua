2f2113e5d128c81359e74c6b9e556453d2c3f331
Styg <kioravuser@gmail.com>
2024-02-05 00:14:06 +0200

    Shorten flash jump keybind, remove other flash keybinds.

--- lua/keybinds/flash.lua
+++ lua/keybinds/flash.lua
@@ -1,10 +1,11 @@
 local nmap = require('utils.keybind').nmap
 
-local function flashmap(left, right, desc)
-  return nmap('<leader>s' .. left, right, { desc = 'Flash ' .. desc })
-end
+-- local function flashmap(left, right, desc)
+--   return nmap('<leader>s' .. left, right, { desc = 'Flash ' .. desc })
+-- end
+--
+-- flashmap('s', function () require('flash').jump() end, 'Jump')
+-- flashmap('S', function () require('flash').treesitter() end, 'Treesitter')
+-- flashmap('r', function () require('flash').treesitter_search() end, 'Treesitter Search')
 
-flashmap('s', function () require('flash').jump() end, 'Jump')
-flashmap('S', function () require('flash').treesitter() end, 'Treesitter')
-flashmap('r', function () require('flash').treesitter_search() end, 'Treesitter Search')
-
+nmap('<leader>s', function () require('flash').jump() end, { desc = 'Flash Jump'})
