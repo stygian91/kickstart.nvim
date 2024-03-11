28754b34410a6c447671433dc113dad49bac7ee0
stygian91 <kioravuser@gmail.com>
2024-03-11 23:53:43 +0200

    Turn off italic comments.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,5 +4,9 @@ return {
   priority = 1000,
   config = function ()
     vim.cmd.colorscheme "catppuccin-mocha"
+
+    -- override colorscheme making comments italic
+    vim.cmd.highlight('Comment gui=none')
+    vim.cmd.highlight('Comment cterm=none')
   end
 }
