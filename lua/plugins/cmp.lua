3c404bf8d8321233698438fe8191f59b343a1dad
Styg <kioravuser@gmail.com>
2024-06-08 23:21:19 +0300

    Fix unintentionally commenting out friendly snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,7 +9,8 @@ return {
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
 
-    -- Adds a number of user-friendly snippets 'rafamadriz/friendly-snippets',
+    -- Adds a number of user-friendly snippets
+    'rafamadriz/friendly-snippets',
   },
 
   config = function()
