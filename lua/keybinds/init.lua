47ccfc504c35148b48141e500ee4f806ed0cf5b0
Atanas Vasilev <atanasvasilev@htmlbox.net>
2024-08-16 10:41:32 +0300

    Add keybind to toggle undotree

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -33,21 +33,25 @@ map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- General
 --------------------------------------------------
 
+-- saving & quitting
 nmap('<leader>q', ':q<cr>', { desc = 'Quit' })
 nmap('<leader>Q', ':qa!<cr>', { desc = 'Force Quit' })
 nmap('<leader>w', ':w<cr>', { desc = 'Write File' })
 nmap('<leader>W', ':w!<cr>', { desc = 'Force Write File' })
 
+-- Oil.nvim file explorer
 nmap('<leader>e', '<CMD>Oil<CR>', { desc = 'Explore' })
 
+-- clipboard
 map('v', '<leader>y', '"*y', { desc = 'Yank into system register' });
 nmap('<leader>p', '"*p', { desc = 'Paste from system register' });
+-- nmap('<leader>d', '"_d', { desc = 'Delete into black hole register'})
 
+-- quickfix navigation
 nmap(']q', ':cnext<cr>', { desc = 'Go to next quickfix item.' })
 nmap('[q', ':cprevious<cr>', { desc = 'Go to prev quickfix item.' })
 
--- nmap('<leader>d', '"_d', { desc = 'Delete into black hole register'})
-
+-- move selected lines up/down and align them horizontally after
 map('v', '<C-j>', ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 map('v', '<C-k>', ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
@@ -55,7 +59,9 @@ map('v', '<C-k>', ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 nmap('<C-d>', '<C-d>zz', { silent = true })
 nmap('<C-u>', '<C-u>zz', { silent = true })
 
-nmap('<A-l>', '<C-w>l')
-nmap('<A-h>', '<C-w>h')
-nmap('<A-j>', '<C-w>j')
-nmap('<A-k>', '<C-w>k')
+-- TODO: figure out an easy keybind that would work on both mac/win/linux
+--
+-- nmap('<A-l>', '<C-w>l')
+-- nmap('<A-h>', '<C-w>h')
+-- nmap('<A-j>', '<C-w>j')
+-- nmap('<A-k>', '<C-w>k')
