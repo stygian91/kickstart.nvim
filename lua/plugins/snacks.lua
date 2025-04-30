98df519cc187e77cfacd3caf533ae25fdd954fd9
Styg <kioravuser@gmail.com>
2025-04-30 23:48:49 +0300

    Enable snacks' image module.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,12 +4,10 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = false },
     explorer = { enabled = false },
+    image = { enabled = true },
     indent = { enabled = false },
     input = { enabled = true },
     picker = { enabled = true },
